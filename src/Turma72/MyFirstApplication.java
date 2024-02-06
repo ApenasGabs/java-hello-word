ba998dd518b26d9d9ca3aaf1de220e1f1c2ff2b9
Gabriel Rodrigues <gabers357@gmail.com>
2024-02-06 15:41:35 -0300

    Add project files and source code

---
+++ src/Turma72/MyFirstApplication.java
@@ -0,0 +1,14 @@
+package Turma72;
+
+public class MyFirstApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nome;
+		nome = "Gabriel";
+		
+		System.out.println("Hello word, do " + nome);
+
+	}
+
+}
